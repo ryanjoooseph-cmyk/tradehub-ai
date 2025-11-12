dcda9b72967c836c98010dc18c158e97b05ac7d2
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2025-11-13 00:14:07 +0800

    Update run_supervisor.py

--- run_supervisor.py
+++ run_supervisor.py
@@ -8,8 +8,8 @@ while True:
     # 2) run one agent task
     subprocess.run(["python", "agent_dispatcher.py"])
 
-    # 3) try to sync (this will now skip if env not set)
-    subprocess.run(["python", "sync_to_github.py"])
+    # 3) (disabled for now) syncing to GitHub
+    # subprocess.run(["python", "sync_to_github.py"])
 
     # 4) sleep
     time.sleep(2)
