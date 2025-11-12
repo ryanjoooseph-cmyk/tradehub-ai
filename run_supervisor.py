12fe90eae6e531ee6ca048e462ecc0234f554941
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2025-11-12 14:39:21 +0800

    Add task management and adjust sleep duration

--- run_supervisor.py
+++ run_supervisor.py
@@ -2,5 +2,11 @@ import time
 import subprocess
 
 while True:
+    # 1) make sure required tasks exist
+    subprocess.run(["python", "feed_from_manifest.py"])
+
+    # 2) run one task
     subprocess.run(["python", "agent_dispatcher.py"])
-    time.sleep(10)
+
+    # 3) pause a bit (you can drop to 2 later)
+    time.sleep(5)
