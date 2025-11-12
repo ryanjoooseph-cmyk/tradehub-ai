4228edbf79850bc7fdc4a16c474d9d4fd95e98c8
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2025-11-12 23:34:03 +0800

    Update run_supervisor.py

--- run_supervisor.py
+++ run_supervisor.py
@@ -2,14 +2,14 @@ import time
 import subprocess
 
 while True:
-    # 1) make sure required tasks exist
-    subprocess.run(["python", "feed_from_manifest.py"])
-
-    # 2) run one task
+    # 1) run one task
     subprocess.run(["python", "agent_dispatcher.py"])
 
-    # 3) sync finished tasks into app repo
-    subprocess.run(["python", "sync_to_github.py"])
+    # 2) (later) feed from manifest
+    # subprocess.run(["python", "feed_from_manifest.py"])
+
+    # 3) (later) sync finished tasks
+    # subprocess.run(["python", "sync_to_github.py"])
 
     # 4) sleep
     time.sleep(2)
