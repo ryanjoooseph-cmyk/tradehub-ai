fa7260485435cfef684befc015984e001cb48d37
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2026-01-02 18:53:21 +0800

    Update getBaseUrl.ts

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -1,7 +1,6 @@
-export function getBaseUrl(): string {
-  const ext = process.env.RENDER_EXTERNAL_URL;
+// lib/getBaseUrl.ts
+export default function getBaseUrl() {
+  const ext = process.env.RENDER_EXTERNAL_URL || process.env.VERCEL_URL;
   if (ext) return ext.startsWith('http') ? ext : `https://${ext}`;
-  const vercel = process.env.VERCEL_URL;
-  if (vercel) return `https://${vercel}`;
   return 'http://localhost:3000';
 }
