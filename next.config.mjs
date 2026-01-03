0c5f20527acd8783accfc99d86e6f802f531f206
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2026-01-03 10:45:21 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: true,
+  experimental: { typedRoutes: true }
 };
+
 export default nextConfig;
