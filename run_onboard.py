69eccdc555e129615aae9bdc0120eb4a1149714b
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2026-01-06 16:41:19 +0800

    Delete run_onboard.py

--- run_onboard.py
+++
@@ -1,8 +0,0 @@
-from crewai import Crew
-from crew_test import onboarding, onboard_task
-
-if __name__ == "__main__":
-    crew = Crew(agents=[onboarding], tasks=[onboard_task])
-    results = crew.kickoff()
-    for r in results:
-        print(r)
