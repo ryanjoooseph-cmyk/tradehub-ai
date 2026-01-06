81f72152b94c24bc17048a697e0859be04c1c9e0
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2026-01-06 16:41:26 +0800

    Delete run_dispute.py

--- run_dispute.py
+++
@@ -1,8 +0,0 @@
-from crewai import Crew
-from crew_test import dispute_agent, dispute_task
-
-if __name__ == "__main__":
-    crew = Crew(agents=[dispute_agent], tasks=[dispute_task])
-    results = crew.kickoff()
-    for r in results:
-        print(r)
