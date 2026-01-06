01bfc8d329fce986d722cd1589323373d5f2bb9f
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2026-01-06 16:40:40 +0800

    Delete run_job_helper.py

--- run_job_helper.py
+++
@@ -1,8 +0,0 @@
-from crewai import Crew
-from crew_test import job_helper, job_task
-
-if __name__ == "__main__":
-    crew = Crew(agents=[job_helper], tasks=[job_task])
-    results = crew.kickoff()
-    for r in results:
-        print(r)
